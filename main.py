69d66e69a9d34473d4e62e345d7ea02daa6432bb
Unknown <vwxyzjn@foxmail.com>
2017-06-08 10:31:11 -0400

    Add traffic data fro different zoom level

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from main_program.HERE_map import ultil
 ## which translate to 34.9237°, -82.4383° according to 
 ## https://msdn.microsoft.com/en-us/library/aa578799.aspx
 
+
 zoom = 14
-(col, row) = get_tile(34.9237, -82.4383, zoom)
-print('furman zoom/col/row is %d/%d/%d' % (zoom,col,row))
+(col, row) = ultil.get_tile(34.9237, -82.4383, zoom)
+print('furman zoom/col/row is %d/%d/%d' % (zoom,round(col),round(row)))
 ## get traffic tile
 #
 # id4 https://1.traffic.maps.cit.api.here.com/maptile/2.1/traffictile/newest/normal.day/14/4440/6494/256/png8?app_id=F8aPRXcW3MmyUvQ8Z3J9&app_code=IVp1_zoGHdLdz0GvD_Eqsw
@@ -40,3 +41,14 @@ quadkey = ultil.get_quadkeys(4440, 6493, zoom)
 #
 # https://traffic.cit.api.here.com/traffic/6.2/flow.json?app_id=F8aPRXcW3MmyUvQ8Z3J9&app_code=IVp1_zoGHdLdz0GvD_Eqsw&quadkey=03200303033202&depth=8bit
 
+
+## get traffic tile of different zoom
+base_url = 'https://1.traffic.maps.cit.api.here.com/maptile/2.1/traffictile/newest/normal.day/'
+app_id = 'F8aPRXcW3MmyUvQ8Z3J9'
+app_code = 'IVp1_zoGHdLdz0GvD_Eqsw'
+image_size = 512  # or could choose 512
+for i in range(20):
+    (col, row) = ultil.get_tile(34.8529419802915, -82.3969868197085, i)  # coffee underground lat and lon
+    total_url = base_url + str(i) + '/' + str(round(col)) + '/' + str(round(row)) + '/' + str(image_size) + '/png8?app_id=' + app_id + '&app_code=' + app_code
+    print(total_url)
+    
