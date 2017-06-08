c4c95eb158de62547449c6ac5136eda6d9a21d8d
Unknown <vwxyzjn@foxmail.com>
2017-06-08 13:40:49 -0400

    measure estimated request of covering greenville for each zoom level

--- main.py
+++ main.py
@@ -41,14 +41,36 @@ quadkey = ultil.get_quadkeys(4440, 6493, zoom)
 #
 # https://traffic.cit.api.here.com/traffic/6.2/flow.json?app_id=F8aPRXcW3MmyUvQ8Z3J9&app_code=IVp1_zoGHdLdz0GvD_Eqsw&quadkey=03200303033202&depth=8bit
 
-
-## get traffic tile of different zoom
-base_url = 'https://1.traffic.maps.cit.api.here.com/maptile/2.1/traffictile/newest/normal.day/'
-app_id = 'F8aPRXcW3MmyUvQ8Z3J9'
-app_code = 'IVp1_zoGHdLdz0GvD_Eqsw'
-image_size = 512  # or could choose 512
-for i in range(20):
-    (col, row) = ultil.get_tile(34.8529419802915, -82.3969868197085, i)  # coffee underground lat and lon
-    total_url = base_url + str(i) + '/' + str(round(col)) + '/' + str(round(row)) + '/' + str(image_size) + '/png8?app_id=' + app_id + '&app_code=' + app_code
-    print(total_url)
+def get_traffic_tile_of_different_zoom():
+    """
+    get traffic tile of different zoom
+    """
+    base_url = 'https://1.traffic.maps.cit.api.here.com/maptile/2.1/traffictile/newest/normal.day/'
+    app_id = 'F8aPRXcW3MmyUvQ8Z3J9'
+    app_code = 'IVp1_zoGHdLdz0GvD_Eqsw'
+    image_size = 512  # or could choose 512
+    for i in range(20):
+        (col, row) = ultil.get_tile(34.8529419802915, -82.3969868197085, i)  # coffee underground lat and lon
+        total_url = base_url + str(i) + '/' + str(round(col)) + '/' + str(round(row)) + '/' + str(image_size) + '/png8?app_id=' + app_id + '&app_code=' + app_code
+        print(total_url)
     
+def measure_estimated_request():
+    """
+    measure the squares/request required to cover greenville county based on zoom level.
+    since we only have 100k request available, we need to carefully arrange our request quantity.
+    """
+    up_left_position = (34.930522, -82.584159)
+    up_right_position = (34.947532,-82.186334)
+    bottom_left_position = (34.595576,-82.547509)
+    for i in range(3,20):
+        up_left_tile = ultil.get_tile(*up_left_position, i)
+        up_right_tile = ultil.get_tile(*up_right_position, i)
+        bottom_left_tile = ultil.get_tile(*bottom_left_position, i)
+        square_x_length = int(up_right_tile[0]) - int(up_left_tile[0]) + 1
+        square_y_length = int(up_left_tile[1]) - int(bottom_left_position[1]) + 1
+        estimated_tile_in_square = square_x_length * square_y_length
+        print(i, estimated_tile_in_square)
+
+
+#get_traffic_tile_of_different_zoom()
+measure_estimated_request()
