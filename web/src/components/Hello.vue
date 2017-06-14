ecd11a5a51dfe3661058205b40567df3671a51db
Unknown <vwxyzjn@foxmail.com>
2017-06-14 12:59:22 -0400

    google maps successfully load geojson

--- web/src/components/Hello.vue
+++ web/src/components/Hello.vue
@@ -1,24 +1,34 @@
 <template>
   <div class="hello">
-    <h1>Hello World</h1>
     <div class="container">
       <div class="map">
         <gmap-map
+          ref = "mymap"
           :center="center"
-          :zoom="7"
+          :zoom="4"
           style="width: 800px; height: 800px"
         >
-          <gmap-marker
+<!--           <gmap-marker
             :key="index"
             v-for="(m, index) in markers"
             :position="m.position"
             :clickable="true"
             :draggable="true"
             @click="center=m.position"
-          ></gmap-marker>
+          ></gmap-marker> -->
+          <gmap-polygon :paths="paths" :editable="true" @paths_changed="updateEdited($event)">
+          </gmap-polygon>
         </gmap-map>
       </div>
+
     </div>
+    <button @click="loadControls">
+      Load Drawing Controls
+    </button>
+    <button @click="displayGeoJson">
+      Display GeoJSON Data
+    </button>
+    <textarea cols="300" rows="50" v-model="geojson"></textarea>
   </div>
 </template>
 
@@ -37,21 +47,35 @@ export default {
   name: 'hello',
   data () {
     return {
-      center: {lat: 10.0, lng: 10.0},
-      markers: [{
-        position: {lat: 10.0, lng: 10.0}
-      }, {
-        position: {lat: 11.0, lng: 11.0}
-      }]
+      center: {lat: -22.14, lng: 123.61},
+      markers: [],
+      paths: [
+          [ {lat: 1.380, lng: 103.800}, {lat: 1.380, lng: 103.810}, {lat: 1.390, lng: 103.810}, {lat: 1.390, lng: 103.800} ],
+          [ {lat: 1.382, lng: 103.802}, {lat: 1.382, lng: 103.808}, {lat: 1.388, lng: 103.808}, {lat: 1.388, lng: 103.802} ]
+      ],
+      geojson: null
+    }
+  },
+  methods: {
+    loadControls() {
+      this.$refs.mymap.$mapObject.data.loadGeoJson('https://storage.googleapis.com/mapsdevsite/json/google.json')
+    },
+    displayGeoJson() {
+      let results
+      this.$refs.mymap.$mapObject.data.toGeoJson((geojson) => {
+        results = JSON.stringify(geojson, null, 2)
+      })
+      this.geojson = results
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!-- Add 'scoped' attribute to limit CSS to this component only -->
 <style scoped>
 .container {
   position: relative;
+  height: 90vh;
 }
 
 .map {
