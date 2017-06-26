ac535f1b16d0ab1eb6386cc352f4c76b332e44bd
Unknown <vwxyzjn@foxmail.com>
2017-06-26 15:24:44 -0400

    Initialized better web UI

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,23 +1,57 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
-  </div>
+  <v-app>
+    <v-navigation-drawer
+      persistent
+      v-model="drawer"
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+        >
+          <v-list-tile value="true">
+            <v-list-tile-action>
+              <v-icon light v-html="item.icon"></v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title v-text="item.title"></v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar light>
+      <v-toolbar-side-icon @click.native.stop="drawer = !drawer" light></v-toolbar-side-icon>
+      <v-toolbar-title v-text="title" ></v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    <main>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </main>
+    <v-footer>
+      <span>&copy; 2017</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
+
 export default {
-  name: 'app'
+  data () {
+    return {
+      drawer: true,
+      items: [
+        { icon: 'bubble_chart', title: 'Inspire' }
+      ],
+      miniVariant: false,
+      title: 'Histraffic'
+    }
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  // @import '../../node_modules/vuetify/dist/vuetify.min.css';
 </style>
