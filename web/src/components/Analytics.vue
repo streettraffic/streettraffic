3c99bbf9d7b058390ad6088b8d603c30598d17eb
Costa Huang <costa.huang@outlook.com>
2017-06-28 00:34:14 -0400

    Made Chart.js work

--- web/src/components/Analytics.vue
+++ web/src/components/Analytics.vue
@@ -10,7 +10,7 @@
         </v-card-row>
         <v-card-text>
           <v-card-row height="auto" center>
-            <div id="container" style="width:100%; height:400px;"></div>
+            <canvas id="myChart" width="400" height="400"></canvas>
           </v-card-row>
         </v-card-text>
       </v-card>
@@ -20,7 +20,11 @@
 
 <script>
 /* eslint-disable */
-var Highcharts = require('highcharts')
+import Chart from 'chart.js'
+import colors from 'nice-color-palettes'
+console.log(colors[0])
+
+// Load module after Highcharts is loaded
 
 export default {
   name: 'Analytics',
@@ -52,29 +56,36 @@ export default {
       return color
     }
   },
-  created() {
-    var myChart = Highcharts.chart('container', {
-        chart: {
-            type: 'bar'
+  mounted() {
+    var ctx = document.getElementById("myChart").getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: colors[0].concat(colors[1]),
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
         },
-        title: {
-            text: 'Fruit Consumption'
-        },
-        xAxis: {
-            categories: ['Apples', 'Bananas', 'Oranges']
-        },
-        yAxis: {
-            title: {
-                text: 'Fruit eaten'
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
             }
-        },
-        series: [{
-            name: 'Jane',
-            data: [1, 0, 4]
-        }, {
-            name: 'John',
-            data: [5, 7, 3]
-        }]
+        }
     });
   }
 }
