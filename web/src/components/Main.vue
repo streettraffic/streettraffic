c0f779b8651d12c7613d6f4c973749b5798d5b13
Unknown <vwxyzjn@foxmail.com>
2017-07-26 10:34:15 -0400

    Install pug templating engine and refactor Main.vue for readibility

--- web/src/components/Main.vue
+++ web/src/components/Main.vue
@@ -1,44 +1,38 @@
-<template>
-  <v-app>
-    <v-navigation-drawer
+<template lang="pug">
+  v-app
+    v-navigation-drawer(
       persistent
       v-model="drawer"
       enable-resize-watcher
       dark
       :mini-variant.sync="mini"
-    >
-      <v-list>
-        <v-list-item
+    )
+      v-list
+        v-list-item(
           v-for="(item, i) in items"
           :key="i"
-        >
-          <v-list-tile value="true" router :to="item.routerAddress" ripple>
-            <v-list-tile-action>
-              <v-icon light v-html="item.icon"></v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title v-text="item.title"></v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar light>
-      <v-toolbar-side-icon @click.native.stop="drawer = !drawer" light></v-toolbar-side-icon>
-      <v-toolbar-title v-text="title" ></v-toolbar-title>
-      <v-spacer></v-spacer>
-    </v-toolbar>
-    <main>
-      <v-container fluid>
-        <transition name="slide" mode="out-in">
-          <router-view></router-view>
-        </transition>
-      </v-container>
-    </main>
-    <v-footer>
-      <span>&copy; 2017</span>
-    </v-footer>
-  </v-app>
+        )
+          v-list-tile(value="true" router :to="item.routerAddress" ripple)
+            v-list-tile-action
+              v-icon(light v-html="item.icon")
+            v-list-tile-content
+              v-list-tile-title(v-text="item.title")
+
+    
+    v-toolbar(light)
+      v-toolbar-side-icon(@click.native.stop="drawer = !drawer" light)
+      v-toolbar-title(v-text="title")
+      v-spacer
+
+
+    main
+      v-container(fluid)
+        transition(name="slide" mode="out-in")
+          router-view
+
+
+    v-footer
+      span &copy; 2017
 </template>
 
 <script>
