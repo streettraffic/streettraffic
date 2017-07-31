844fba29ab5ac2ff868fac9f9084f53922715528
Unknown <vwxyzjn@foxmail.com>
2017-07-31 17:29:25 -0400

    Added quantile support for analytics

--- web/src/components/Analytics/TrafficPattern.vue
+++ web/src/components/Analytics/TrafficPattern.vue
@@ -6,60 +6,51 @@
         | In this section, you can pick a date at the calendar below and the site will generate a graph of average jamming factor of the city you are monitoring
       p Now pick a city that we monitored
       v-select.input-group--focused(v-bind:items='analytics_monitored_area_description_collection' v-model='selected_area_description' label='Select')
-    v-flex(xs12 md6)
-      v-card
-        v-card-row.green.darken-1
-          v-card-title
-            span.white--text Quartile
-            v-spacer
-        v-card-text
-          v-card-row(height='auto' center)
-            canvas#quartile(width='400' height='400')
+    
+    chart(:data='quantileData' :chartTitle="'Quantile'" chartId='quantile')
+
     v-flex.ma-3(xs12 md5)
       h6 Pick a date here to see traffic patterns
       v-date-picker(v-model='datePicker')
       div(v-show='true') {{datePicked}}
-    
-    
-    v-flex(xs12 md6).mt-5
-      v-card
-        v-card-row.green.darken-1
-          v-card-title
-            span.white--text Mean and Standard Deviation
-            v-spacer
-        v-card-text
-          v-card-row(height='auto' center)
-            canvas#std(width='400' height='400')
+
+    chart(:data='meanData' :chartTitle="'Mean and Standard Deviation'" chartId='mean').mt-5
 </template>
 
 <script>
-import Chart from 'chart.js'
+import Chart from '../Chart.vue'
 import colors from 'nice-color-palettes'
 
 // Load module after Highcharts is loaded
 
 export default {
   name: 'Analytics',
+  components: {
+    Chart
+  },
   data: function () {
     return {
       selected_area_description: null,
       analytics_monitored_area_description_collection: [],
-      data: {
-        labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
-        series: [
-            [12, 9, 7, 8, 5],
-            [2, 1, 3.5, 7, 3],
-            [1, 3, 4, 5, 6]
-        ]
-      },
-      options: {
-        fullWidth: true,
-        chartPadding: {
-          right: 40
-        }
-      },
       datePicker: null,
-      chart: null   // initiated by mounted, a Chart.js chart
+      quantileData: {
+        labels: [],
+        datasets: [{
+          label: 'Jamming Factor',
+          data: [],
+          borderColor: '#3498db',
+          fill: false
+        }]
+      },
+      meanData: {
+        labels: [],
+        datasets: [{
+          label: 'Jamming Factor',
+          data: [],
+          borderColor: '#3498db',
+          fill: false
+        }]
+      }
     }
   },
   methods: {
@@ -83,50 +74,61 @@ export default {
         let labels = traffic_pattern.map((item) => {
           return new Date(item.crawled_timestamp).toLocaleTimeString()
         })
-        let data = traffic_pattern.map((item) => item.average_JF)
-        let upper = traffic_pattern.map((item) => item.average_JF + item.standard_deviation_JF)
-        let lower = traffic_pattern.map((item) => item.average_JF - item.standard_deviation_JF)
-        self.buildChart('std', labels, data, upper, lower)
+        self.createMeanAndStandardDeviation(traffic_pattern, labels)
+        self.createQuantile(traffic_pattern, labels, 0.90, 0.50, 0.10)
       }
     },
-    buildChart(id, labels, data, upper, lower) {
-      let ctx = document.getElementById(id).getContext('2d')
-      if (this.chart) {
-        this.chart.destroy()
-      }
-      this.chart = new Chart(ctx, {
-        type: 'line',
-        data: {
-          labels: labels,
-          datasets: [{
-            label: 'Jamming Factor',
-            data: data,
-            borderColor: '#3498db',
-            fill: false
-          },
-          {
-            label: 'Error upper',
-            data: upper,
-            borderColor: '#FF5733',
-            fill: false
-          },
-          {
-            label: 'Error lower',
-            data: lower,
-            borderColor: '#FF5733',
-            fill: false
-          }]
+    createMeanAndStandardDeviation(traffic_pattern, labels) {
+      let data = traffic_pattern.map((item) => item.average_JF)
+      let upper = traffic_pattern.map((item) => item.average_JF + item.standard_deviation_JF)
+      let lower = traffic_pattern.map((item) => item.average_JF - item.standard_deviation_JF)
+      this.meanData = {
+        labels: labels,
+        datasets: [{
+          label: 'Jamming Factor',
+          data: data,
+          borderColor: '#3498db',
+          fill: false
         },
-        options: {
-          scales: {
-            yAxes: [{
-              ticks: {
-                beginAtZero: true
-              }
-            }]
-          }
-        }
-      })
+        {
+          label: 'Error upper',
+          data: upper,
+          borderColor: '#FF5733',
+          fill: false
+        },
+        {
+          label: 'Error lower',
+          data: lower,
+          borderColor: '#FF5733',
+          fill: false
+        }]
+      }
+    },
+    createQuantile(traffic_pattern, labels, percentile1, percentile2, percentile3) {
+      let upper = traffic_pattern.map((item) => item['JF_collection'][Math.floor(percentile1 * traffic_pattern.length)])
+      let middle = traffic_pattern.map((item) => item['JF_collection'][Math.floor(percentile2 * traffic_pattern.length)])
+      let lower = traffic_pattern.map((item) => item['JF_collection'][Math.floor(percentile3 * traffic_pattern.length)])
+      this.quantileData = {
+        labels: labels,
+        datasets: [{
+          label: (percentile1 * 100).toFixed(1) + '%',
+          data: upper,
+          borderColor: '#FF5733',
+          fill: false
+        },
+        {
+          label: (percentile2 * 100).toFixed(1) + '%',
+          data: middle,
+          borderColor: '#3498db',
+          fill: false
+        },
+        {
+          label: (percentile3 * 100).toFixed(1) + '%',
+          data: lower,
+          borderColor: '#FF5733',
+          fill: false
+        }]
+      }
     }
   },
   computed: {
@@ -147,8 +149,6 @@ export default {
       self.analytics_monitored_area_description_collection = JSON.parse(event.data)
       console.log(self.analytics_monitored_area_description_collection)
     }
-    this.buildChart('std', [], [])
-    this.buildChart('quartile', [], [])
   }
 }
 </script>
